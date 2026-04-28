9087e88b977b8032a4f620cf288c47404f7b4264
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2026-04-28 08:46:35 -0300

    feat: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./core/dashboard/dashboard'),
+  },
+  {
+    path: 'departamento',
+    loadComponent: () => import('./pages/departamento/departamento'),
+  },
+  {
+    path: 'empresa',
+    loadComponent: () => import('./pages/empresa/empresa'),
+  },
+  {
+    path: 'funcionario',
+    loadComponent: () => import('./pages/funcionario/funcionario'),
+  },
+  {
+    path: 'prodes',
+    loadComponent: () => import('./pages/prodes/prodes'),
+  },
+  {
+    path: 'tabela',
+    loadComponent: () => import('./pages/tabela/tabela'),
+  },
+];
