703328be3d754749ebd8aa440e239dc4a8a0a70f
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2026-05-24 09:22:13 -0300

    feat: store

--- src/app/core/components/mini-card.ts
+++ src/app/core/components/mini-card.ts
@@ -1,9 +1,8 @@
 import { TitleCasePipe } from "@angular/common";
-import { Component, input, signal } from "@angular/core";
+import { Component, input } from "@angular/core";
 import { MatBadgeModule } from "@angular/material/badge";
 import { MatCardModule } from "@angular/material/card";
 import { MatIcon } from "@angular/material/icon";
-import { TIME_DELAY } from "../shared/consts";
 import { CountUpDirective } from "../shared/directives/count-up.directive";
 
 @Component({
@@ -24,9 +23,10 @@ import { CountUpDirective } from "../shared/directives/count-up.directive";
           </span>
         </mat-card-title>
         <mat-card-subtitle class="self-end">
-          @if (valor2() !== false) {
-            <div class="font-bold" [appCountUpDirective]="valor()" [duration]="2">{{ valor() }}</div>
-            <!-- <div class="font-bold">{{ valor() }}</div> -->
+          @if (valor() !== false) {
+            <div class="font-bold" [appCountUpDirective]="valor()" [duration]="2">
+              {{ valor() }}
+            </div>
           } @else {
             <div class="animate-spin size-6 flex items-center justify-center">
               <mat-icon>change_circle</mat-icon>
@@ -46,13 +46,8 @@ import { CountUpDirective } from "../shared/directives/count-up.directive";
 export class MiniCard {
   icone = input.required<string>();
   title = input.required<string>();
-  valor = input.required<any>();
+  valor = input<any>(false);
   bg = input<string>('bg-blue-900');
   text = input<string>('text-gray-100');
-  valor2 = signal<any>(false);
-  constructor() {
-    setTimeout(() => {
-      this.valor2.set(this.valor());
-    }, TIME_DELAY * 10);
-  }
+
 }
--- src/app/pages/funcionario/funcionario-card.ts
+++ src/app/pages/funcionario/funcionario-card.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from "@angular/core";
+import { Component, effect, inject, signal } from "@angular/core";
 import { MiniCard } from "../../core/components/mini-card";
+import { TIME_DELAY } from "../../core/shared/consts";
 import { FuncionarioStore } from "./shared/funcionario.store";
 
 @Component({
@@ -10,18 +11,17 @@ import { FuncionarioStore } from "./shared/funcionario.store";
       <app-mini-card
         icone="person_add"
         title="ativos"
-        [valor]="funcionarioStore.totalAtivos().length"
+        [valor]="totalAtivos()"
         bg="bg-linear-to-b from-blue-400 via-blue-600 to-blue-800"
         text="text-white"
-
       />
       <!-- [valor]="funcionarioStore.totalSalarioBase() | currency: 'BRL'" -->
       <app-mini-card
         icone="attach_money"
         title="total salario base"
-        [valor]="funcionarioStore.totalSalarioBase()"
+        [valor]="totalSalarioBase()"
         bg="bg-linear-to-b from-green-400 via-green-600 to-green-800"
-        text="text-white"        
+        text="text-white"
       />
     </div>
   `,
@@ -33,4 +33,17 @@ import { FuncionarioStore } from "./shared/funcionario.store";
 })
 export default class FuncionarioCard {
   funcionarioStore = inject(FuncionarioStore);
+  totalSalarioBase = signal<any>(false);
+  totalAtivos = signal<any>(false);
+
+  constructor() {
+    effect(() => {
+      this.totalAtivos.set(false);
+      this.totalSalarioBase.set(false);
+      setTimeout(() => {
+        this.totalAtivos.set(this.funcionarioStore.totalAtivos().length);
+        this.totalSalarioBase.set(this.funcionarioStore.totalSalarioBase());
+      }, TIME_DELAY);
+    });
+  }
 }
