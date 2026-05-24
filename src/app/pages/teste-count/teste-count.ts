96dc9e13f81e5df16bfe334d47a6b58e555ccd01
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2026-05-24 11:34:45 -0300

    feat: count up sem directiva

--- src/app/pages/teste-count/teste-count.ts
+++ src/app/pages/teste-count/teste-count.ts
@@ -1,17 +1,11 @@
-import { Component, ViewChild, signal } from "@angular/core";
-import { MatButton } from "@angular/material/button";
-import { CountUp } from "countup.js";
-import { CountUpDirective } from "../../core/shared/directives/count-up.directive";
+import { Component, signal } from "@angular/core";
 
 @Component({
   selector: 'app-teste-count',
-  imports: [CountUpDirective, MatButton],
+  imports: [],
 
   template: `
-    <div class="flex flex-col">
-      <span id="myCountUp" [countUp]="valor()" [duration]="2" class="border p-4 rounded-2xl"></span>
-      <button matButton="filled" (click)="onReset()">reset</button>
-    </div>
+  <p>{{ title() }}</p>
   `,
   styles: `
     :host {
@@ -21,11 +15,5 @@ import { CountUpDirective } from "../../core/shared/directives/count-up.directiv
   `,
 })
 export default class TesteCount {
-  valor = signal<number>(5000);
-
-  @ViewChild('myCountUp') countUpRef?: CountUpDirective
-  onReset(){
-    const myCountUp = new CountUp('myCountUp', 50000, { duration: 2 });
-    myCountUp.start();
-  }
+  title = signal<string>('app-teste-count');
 }
